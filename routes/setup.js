aef0ca86ff259bf96d8ee62d387f96c568d0da96
toniriyanov11 <toniriyanov@gmail.com>
2020-10-24 00:35:29 +0700

    style: add setup CSR form

--- routes/setup.js
+++ routes/setup.js
@@ -27,13 +27,34 @@ router.post('/setupharga/edit', async function(req, res, next) {
 });
 
 
- // setup harga 
+ // setup harga barang return
  router.get('/setuphargabarangreturn',async function(req, res, next) {
    res.render('index', { title: 'Setup Harga Barang Return', page:'setup-data/setuphargabarangreturn.ejs'});
 })
 
 
 router.post('/setuphargabarangreturn/edit', async function(req, res, next) {
+   try {
+      var data = req.body.data
+      let dataResponse = await controllerMaster.updateMasterVarianBahan(data)
+      if (dataResponse.success) {
+         res.status(200).json(dataResponse);
+      } else {
+         res.status(400).json();
+      }
+   } catch (err) {
+      res.status(500).json(err);
+   }
+});
+
+
+ // setup harga csr
+ router.get('/setupcsr',async function(req, res, next) {
+   res.render('index', { title: 'Setup CSR', page:'setup-data/setupcsr.ejs'});
+})
+
+
+router.post('/setupcsr/edit', async function(req, res, next) {
    try {
       var data = req.body.data
       let dataResponse = await controllerMaster.updateMasterVarianBahan(data)
