512d0c256bc29fc7feef9ecd11153f42f8d59472
toniriyanov11 <toniriyanov@gmail.com>
2020-10-24 21:01:17 +0700

    fix: fix data barang jadi

--- models/modelDataProduksi.js
+++ models/modelDataProduksi.js
@@ -380,8 +380,9 @@ router.getBarangJadiById = function(id) {
 }
 
 router.insertBarangJadi = function(data) {
+    console.log(data)
     return new Promise((resolve, reject) => {
-        database.getConnection().query(`CALL P_BARANG_JADI(?,?,?,?,?,?,?,?,?,?,?,?,?)`,[data.tanggal, data.nama, data.hargaPerPcs, data.ket , data.tglSekarang , data.item, data.biayaTambahan.tieDye, data.biayaTambahan.label, data.biayaTambahan.handtag, data.biayaTambahan.packing, data.biayaTambahan.dllProduksi, data.biayaTambahan.dllAksesoris, data.jumlah],(err,results) => {
+        database.getConnection().query(`CALL P_BARANG_JADI(?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,[data.tanggal, data.nama, data.hargaPerPcs, data.ket , data.tglSekarang , data.item, data.biayaTambahan.tieDye, data.biayaTambahan.label, data.biayaTambahan.handtag, data.biayaTambahan.packing, data.biayaTambahan.dllProduksi, data.biayaTambahan.dllAksesoris, data.jumlah, data.hargaPokok],(err,results) => {
             if (err) {
                 database.getConnection().query(`ROLLBACK;`)
                 console.log(err)
